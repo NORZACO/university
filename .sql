5b32a868a0f147e691ea75882f88e9b332ef7b6d
Mwamuzi Shadrick <norzaco@gmail.com>
2023-03-15 13:24:31 +0100

    first commit

---
+++ .sql
@@ -0,0 +1,17 @@
+
+
+
+INSERT INTO Schools (Name, Address, Description)
+Values ('Hogwart', 'Highlands', 'School for Wizards');
+
+
+
+INSERT INTO Students (FirstName, LastName, SchoolId)
+Values ('Harry', 'Potter', 1);
+
+
+
+SELECT * FROM Students;
+
+
+SELECT * FROM Schools;
---
+++ app.js
@@ -0,0 +1,42 @@
+require('dotenv').config()
+var db = require("./models");
+db.sequelize.sync({ force: false })
+
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+const GraphQL = require('graphql');
+const { GraphQLID, GraphQLNonNull, GraphQLObjectType, GraphQLSchema } = GraphQL;
+
+const studentType = require('./types/studentType')(GraphQL);
+const StudentService = require('./services/studentService');
+const studentService = new StudentService(db);
+
+const RootQuery = new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+        getStudent: {
+            type: studentType,
+            args: { id: { type: new GraphQLNonNull(GraphQLID) } }
+        }
+    }
+})
+const schema = new GraphQLSchema({
+    query: RootQuery
+})
+
+var root = {
+    getStudent: async ({ id }) => await studentService.get(id),
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port http://localhost:${process.env.PORT}/graphql`);
+})
